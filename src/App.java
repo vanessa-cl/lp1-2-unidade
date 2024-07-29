0ba7ab12dde58ece3ad07a199ca3ea040ef66ce1
Vanessa Lima <vanessalima951@gmail.com>
2024-07-29 15:48:50 -0300

    adicionando tratamento de exceção customizada

--- src/App.java
+++ src/App.java
@@ -4,15 +4,19 @@ public class App {
         Profissional p1 = new Profissional("Fulano", "123.456.789-00", 100.0, 40, "Sei la");
         Estudante e1 = new Estudante("Vanessa", "099.456.789-60", 100.0, 23, "IFBA");
         Profissional p2 = new Profissional("Fulana", "009.456.789-03", 100.0, 59, "Num sei");
-        Estudante e2 = new Estudante("Ana", "109.456.789-03", 100.0, 20, "UFBA");
+        Estudante e2 = new Estudante("Ana", "009.456.789-03", 100.0, 20, "UFBA");
 
         p1.desconto();
         bd.inserir(p1);
         bd.inserir(e1);
         bd.inserir(p2);
-        bd.inserir(e2);
+        // exemplo de tratamento da exceção customizada
+        try {
+            bd.inserir(e2);
+        } catch (CCException e) {
+            System.out.println(e);
+        }
         bd.remover(p2.cpf);
-
         bd.maisVelho().imprimir();
     }
 }
